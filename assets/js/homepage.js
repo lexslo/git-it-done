b291aa60ef083a7c4fbad93bb783eed9dc79430a
Sparkle Party <lexi.slovik@gmail.com>
2021-12-02 17:11:02 -0800

    add user repos and display to page, check for errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,82 @@
+var userFormEl = document.getElementById("user-form");
+var nameInputEl = document.getElementById("username");
+var repoContainerEl = document.getElementById("repos-container");
+var repoSearchTerm = document.getElementById("repo-search-term");
+
 var getUserRepos = function (user) {
 
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    fetch(apiUrl).then(function(response) {
-            response.json().then(function(data) {
-                console.log(data);
-            });
-    });
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: GitHub User Not Found");
+            }
+        })
+        .catch(function(error) {
+            // .catch() chained to .then()
+            alert("Unable to connect to GitHub");
+        })
 };
 
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a Github username.")
+    }
+}
+
+var displayRepos = function (repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found";
+        return;
+    }
+
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item felx-row justify-space-between align-center";
+        // create a span element to hold repo name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+        // check if repo has open issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issues(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+        // append to container
+        repoEl.appendChild(statusEl);
+
+        // append container to DOM
+        repoContainerEl.appendChild(repoEl);
+    }
+
+}
+
 getUserRepos();
+
+userFormEl.addEventListener("submit", formSubmitHandler);
